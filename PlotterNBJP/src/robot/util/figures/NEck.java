fe3c2dd1b832d69f346b50251115bcafbd0fb48f
Noah Ruben <noah.ruben@online.de>
2018-11-27 11:35:11 +0100

    fix neck

--- PlotterNBJP/src/robot/util/figures/NEck.java
+++ PlotterNBJP/src/robot/util/figures/NEck.java
@@ -9,36 +9,33 @@ import robot.util.Coordinate;
 import robot.util.EinheitsKreis;
 import robot.util.Instruction;
 
-public class NEck extends GeoFig {
+public class NEck extends GeoFig{
 
 	private double degree;
 	private int anzahlEcken;
 
-	public NEck(double degree, Coordinate mittelpunkt, Roboter roboter, double radius, int anzahlEcken) {
+	public NEck(double degree, Coordinate mittelpunkt, Roboter roboter, double radius, int anzahlEcken){
 		super(mittelpunkt, roboter, radius);
 		this.degree = degree;
 		this.anzahlEcken = anzahlEcken;
 	}
 
 	@Override
-	public List<Instruction> getInstructionSet() {
+	public List<Instruction> getInstructionSet(){
 		ArrayList<Instruction> instructions = new ArrayList<Instruction>();
 		// goToMittelpunkt
-		instructions.add(new Instruction(false,
-				this.getMittelpunkt().getxCoord() - this.getRoboter().getCurrentCoordinate().getxCoord(),
-				this.getMittelpunkt().getyCoord() - this.getRoboter().getCurrentCoordinate().getyCoord()));
-		//Prepare the Drawing
+		instructions.add(new Instruction(false, this.getMittelpunkt().getxCoord() - this.getRoboter().getCurrentCoordinate().getxCoord(), this.getMittelpunkt().getyCoord() - this.getRoboter().getCurrentCoordinate().getyCoord()));
+		// Prepare the Drawing
 		BigDecimal nDegree = new BigDecimal(360).divide(new BigDecimal(this.anzahlEcken));
 		Coordinate curCoor = new Coordinate(0, 0);
-		//First Coord of the Necks with the degree offset
-		Coordinate nextCoor = EinheitsKreis.berechnePositionAufKreis(new Coordinate(getRadius(), 0), this.degree);
+		// First Coord of the Necks with the degree offset
+		Coordinate nextCoor = EinheitsKreis.berechnePositionAufKreis(new Coordinate(this.getRadius(), 0), this.degree);
 		instructions.add(new Instruction(false, 0, nextCoor.getyCoord()));
 		System.out.println("deg: " + nDegree);
-		for (int i = 1; i <= this.anzahlEcken; i++) {
+		for(int i = 1; i <= this.anzahlEcken; i++){
 			curCoor = new Coordinate(nextCoor.getxCoord(), nextCoor.getyCoord());
-			nextCoor = EinheitsKreis.berechnePositionAufKreis(curCoor, nDegree.intValue());
-			Coordinate deltaCoor = new Coordinate(curCoor.getxCoord() - nextCoor.getxCoord(),
-					curCoor.getyCoord() - nextCoor.getyCoord());
+			nextCoor = EinheitsKreis.berechnePositionAufKreis(curCoor, nDegree.doubleValue());
+			Coordinate deltaCoor = new Coordinate(curCoor.getxCoord() - nextCoor.getxCoord(), curCoor.getyCoord() - nextCoor.getyCoord());
 			instructions.add(new Instruction(true, deltaCoor.getxCoord(), deltaCoor.getyCoord()));
 
 //			instructions.add(new Instruction(true, this.getRadius(),0));
